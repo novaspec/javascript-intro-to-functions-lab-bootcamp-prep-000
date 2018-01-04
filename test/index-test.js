542857ad7a07d2de5a7abbf9b3bd533b903348a6
Jonatan Kaplan <jonatankaplan@gmail.com>
2018-01-04 16:52:08 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,5 +1,5 @@
 
-describe('shout(string)', () {
+describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO')
   })
